aeb266b31e86747345491aa2b3378b3effec21bb
kokko.sampo@gmail.com <kokko.sampo@gmail.com>
2023-11-10 01:15:38 +0200

    fixed renaming of files when filename already exists in destination attempt #2

--- main.py
+++ main.py
@@ -86,9 +86,10 @@ if download_path != "":
                 new_filename = os.path.join(instructions[extension], d)
                 while os.path.isfile(new_filename):
                     new_filename_parts = new_filename.split(".")
-                    new_filename = "{}_copy.{}".format(
-                        new_filename_parts[0:-1].join(),
-                        new_filename_parts[-1])
+                    new_filename = ""
+                    for part in new_filename_parts[0:-1]:
+                        new_filename.append(part)
+                    new_filename.append("_copy.{}".format(new_filename_parts[-1]))
                 # If the destination and origin are on the same drive
                 if old_filename.split(":")[0] == new_filename.split(":")[0]:
                     os.rename("{}".format(old_filename), "{}"
